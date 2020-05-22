934daa68e242202527553a8425b36f8667f3021e
Hossein2x <hrakbari77@gmail.com>
2020-05-22 23:53:06 +0430

    all done

--- square_root.py
+++ square_root.py
@@ -14,8 +14,9 @@ def dou_maker(arr):
     while (counter < len(arr)):
         dou = []
         val = arr[counter]
-        dou.append(val)
         dou.append(counter)
+        dou.append(val)
+        
         result_arr.append(dou)
         counter += 1
     return result_arr
@@ -125,7 +126,7 @@ def odd_roots(odd_cycle):
     return root
 
 
-def even_root(even_cycle1, even_cycle2):
+def even_roots(even_cycle1, even_cycle2):
 
     counter = 0
     root =[]
@@ -138,47 +139,95 @@ def even_root(even_cycle1, even_cycle2):
 
 
 
-def roots_finder(sample):
+def rootsFinder(cycle_sample):  
 
-    final_roots = []
+    roots = []   
+    while(len(cycle_sample) != 0):
+        if(len(cycle_sample[0]) % 2 ==0):
+            counter =1
+            while ( counter < len(cycle_sample)):
 
-
-
-        
-        
-
-
-# dataFile = open("data.in","r")
-# data_lines = dataFile.readlines()
-# sample_numbers = int(data_lines[0])
-# i = 0
-# n_list = list()
-# while i<sample_numbers:
-#     index = (2*(i+1))-1
-#     n = int(data_lines[index])
-#     m_list = list()
-#     counter = 0
-#     line = data_lines[index+1].split()
-#     while counter<n:
-#         m_list.append(int(line[counter]))
-#         counter+=1
-#     n_list.append(m_list)
-#     i+=1
+                if(len(cycle_sample[0])== len(cycle_sample[counter])):
+                    even_root = even_roots(cycle_sample[0], cycle_sample[counter])
+                    cycle_sample.pop(counter)
+                    cycle_sample.pop(0)
+                    roots.append(even_root)
+                counter += 1
+        else:
+            odd_root = odd_roots(cycle_sample[0])
+            cycle_sample.pop(0)
+            roots.append(odd_root)
+    return roots
+# print(rootsFinder([[1,2,0]]))
+def squareRootExe(sample_orig):
+    '''
+        sample_orig = [2,3,0,1]
+    '''
+    sample_cycles = all_cycles(dou_maker(sample_orig))
+    cycles_roots = rootsFinder(sample_cycles)
+    answer = []
+    counter = 0
+    while(counter < len(sample_orig)): #initialing with 0
+        answer.append(0)
+        counter += 1 
     
-# counter3 = 0
-# samples_cycle = []
+    counter = 0
+    while(counter < len(cycles_roots)):
 
-# while(counter3 < len(n_list)):
-#     samples_cycle.append(all_cycles(dou_maker(n_list[counter3])))
-#     counter3 +=1
+        counter1 = 0
+        while(counter1 < len(cycles_roots[counter])):
+            if(counter1 == len(cycles_roots[counter]) -1 ): # [[1,2,0]]
+                val = cycles_roots[counter][counter1]
+                answer[cycles_roots[counter][0]] = sample_orig[val]
+            else:
+                val = cycles_roots[counter][counter1]
+                answer[cycles_roots[counter][counter1+1]] = sample_orig[val]
+            counter1 += 1
+        counter += 1
 
-# counter3 =0
-# while(counter3 < len(samples_cycle)):
+    return answer
+
+
+dataFile = open("data.in","r")
+data_lines = dataFile.readlines()
+sample_numbers = int(data_lines[0])
+i = 0
+n_list = list()
+while i<sample_numbers:
+    index = (2*(i+1))-1
+    n = int(data_lines[index])
+    m_list = list()
+    counter = 0
+    line = data_lines[index+1].split()
+    while counter<n:
+        m_list.append(int(line[counter]))
+        counter+=1
+    n_list.append(m_list)
+    i+=1
     
-#     if( not impossible_finder(samples_cycle[counter3]) ):
-#         print ("sHomare " + str(counter3+1))
+counter3 = 0
+samples_cycle = []
+
+while(counter3 < len(n_list)):
+    samples_cycle.append(all_cycles(dou_maker(n_list[counter3])))
+    counter3 +=1
+
+counter3 =0
+while(counter3 < len(samples_cycle)):
+
+    if(counter3 == 13):
+        break
+    if( not impossible_finder(samples_cycle[counter3]) ):
+        print ("sHomare " + str(counter3+1) + ", ans : " + "Impossible")
     
-#     counter3 +=1
+    else:
+        ans = squareRootExe(n_list[counter3])
+        print("shomare " + str(counter3 + 1 ) + ", ans :" +str(ans))
+    counter3 +=1
+    print("\n")
+
+
+
 
 
 
