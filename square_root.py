0a2c3875eaec65d4b63a6666bb42d84153bb19f8
Hossein2x <hrakbari77@gmail.com>
2020-05-22 11:16:26 +0430

    function dou added

--- square_root.py
+++ square_root.py
@@ -1,5 +1,28 @@
 
 
+
+
+def dou_maker(arr):
+    counter = 0
+    result_arr = []
+    dou = []
+    while (counter < len(arr)):
+        val = arr[counter]
+        dou.append(val)
+        dou.append(counter)
+        result_arr.append(dou)
+        counter += 1
+    return result_arr
+
+
+def cycle_finder(arr):
+    result_arr =[]
+    cycle_arr = []
+    counter = 0
+
+    while ( counter < len(arr)):
+
+
 dataFile = open("data.in","r")
 data_lines = dataFile.readlines()
 sample_numbers = int(data_lines[0])
@@ -12,9 +35,5 @@ while(counter < len(data_lines)):
     elements = data_lines[counter]
     one_elements_arr.append(elements)
     counter += 1
-
-
-def cycle_finder(arr):
-
     
 print(one_elements_arr)
